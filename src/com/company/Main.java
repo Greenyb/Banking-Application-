9a075b8de48c4d988389762f28444d2ee668ea3f
Feldwerth <mfeldwerth@my.stlcc.edu>
2021-06-08 09:34:39 -0500

    add fix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("ⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢ");
+        System.out.println("ⅢWelcome to the probie banking application!Ⅲ");
+        System.out.println("ⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢⅢ");
+
+        System.out.println("Main menu");
+        // maybe add a user interaction application
+        System.out.println("enter option");
+        //A driver class must be created to run the program and interact through the command line
+        //Method to create, update, read, and delete account application
+        //organization by adding arrays and tables to hold the information.
+        //set up folders as a bridge or connection between each account that holds all the information inside of it.
+
+        //Account ids must be in a random sequence or order
+        // methods to save current account information to a .csv file
     }
 }
