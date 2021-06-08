dabf8080c12f5586d69f176b3966fce96ad2bc43
EFMcMahon <emcmahon8962@gmail.com>
2021-06-08 13:10:32 -0500

    EricAsDriver

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -10,6 +13,28 @@ public class Main {
         System.out.println("Main menu");
         // maybe add a user interaction application
         System.out.println("enter option");
+        System.out.println("R: read bank info\nC:create account\nU: update account\nD: delete account");
+        Scanner option = new Scanner(System.in);
+        String chosen = option.nextLine();
+        int arr[][] = ;
+        if(chosen == "R"){
+
+        }else if(chosen == "C"){
+            Random rand = new Random();
+            System.out.println("Enter your name: ");
+            Scanner name = new Scanner(System.in);
+            String userName = name.nextLine();
+            System.out.println("What type of account?");
+            String accType = name.nextLine();
+            int accNum = rand.nextInt(10000000);
+
+        }else if(chosen == "U"){
+
+        }else if(chosen == "D"){
+
+        }else{
+            System.out.println("Invalid option");
+        }
         //A driver class must be created to run the program and interact through the command line
         //Method to create, update, read, and delete account application
         //organization by adding arrays and tables to hold the information.
